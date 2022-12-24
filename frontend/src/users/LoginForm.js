ed730c71bd29db3994032fbbfc219e2cfa1643bb
JCHammer18 <101356353+JCHammer18@users.noreply.github.com>
2022-12-24 03:04:22 -0500

    Update LoginForm.js

--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -18,25 +18,26 @@ function LoginForm() {
       
   
     ___
-    async function handleSubmit(e) {
-        const response = await fetch(`http://localhost:5000/authentication/`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(credentials)
-        })
-    
-        const data = await response.json()
-    
-        if (response.status === 200) {
-            setCurrentUser(data.user)
-            console.log(data.token)
-            history.push(`/`)
-        } else {
-            setErrorMessage(data.message)
-        }
-    
+    ___
+async function handleSubmit(e) {
+    const response = await fetch(`http://localhost:5000/authentication/`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(credentials)
+    })
+
+    const data = await response.json()
+
+    if (response.status === 200) {
+        setCurrentUser(data.user)
+        localStorage.setItem('token', data.token)
+        history.push(`/`)
+    } else {
+        setErrorMessage(data.message)
+    }
+
     
     console.log(data)
 }
